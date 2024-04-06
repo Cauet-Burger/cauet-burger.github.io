c5a0472166fcba766b26b185d069dadaeea7d1c9
Dhalian <67284307+Dhalian@users.noreply.github.com>
2024-04-06 19:45:20 +0200

    Delete niggy_converter directory

--- niggy_converter/js/script.js
+++
@@ -1,9 +0,0 @@
-function conversion()
-{
-    var bien = document.getElementById("bien");
-    const img = document.getElementById("img");
-    var convert = document.getElementById("convert")
-    img.src = 'https://media.gqmagazine.fr/photos/5b9926f30c3bca00115f4798/master/w_320%2Cc_limit/chauves_et_beaux_837788172.jpg'
-    bien.innerHTML = "BIEN";
-
-}
